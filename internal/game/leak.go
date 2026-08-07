2eb611bc64b779576b370e3ceabf1048927984a7
garnizeH <garnizeh.dev@gmail.com>
2026-08-07 18:59:35 -0300

    DEMO: a committed connection string must be rejected

---
+++ internal/game/leak.go
@@ -0,0 +1,4 @@
+package game
+
+const databaseURL = "postgres://cinzal:s3cr3t-p4ssw0rd@db.example.com:5432/cinzal"
+const sessionKey = "9f2b7c4e1a8d3f6b0c5e2a9d7f4b1c8e6a3d0f7b2c9e5a1d8f4b7c0e3a6d9f2b"
